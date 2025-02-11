8f88b7bd088fa5af8295e57d35898a8e836e3c67
Ayaan <me.ayaan021@gmail.com>
2025-02-11 16:58:47 +0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Add smooth scroll to anchor links for better UX
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
